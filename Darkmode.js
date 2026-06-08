d4eb0355fcfc71ba12222e3cb35d7bb183e1228d
Nicolas Siqueira <109821803+Nicolassiqueira05@users.noreply.github.com>
2026-06-08 18:16:34 -0300

    Update Darkmode.js

--- Darkmode.js
+++ Darkmode.js
@@ -1,4 +1,16 @@
+let ls = localStorage;
+
+if(ls.getItem("dark")){
+  document.querySelector("#darkmode").checked = ls.getItem("dark");
+}
+
 document.querySelector("#darkmode")
   .addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
+    ls.setItem("dark", document.querySelector("#darkmode").checked)
 });
+
+
+
+
+
